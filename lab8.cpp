ff807e73dddbb27b3a4afec4d5569cdf5e35aa47
Venuri Amarasinghe <amarasingheamvm.20@uom.lk>
2023-05-02 17:24:58 +0530

    my code 1

--- lab8.cpp
+++ lab8.cpp
@@ -2,9 +2,36 @@
 using namespace std;
   
 // function to heapify the tree
+void swap(int* a, int* b)
+{
+ 
+    int temp = *a;
+ 
+    *a = *b;
+ 
+    *b = temp;
+} 
+
 void heapify(int arr[], int n, int root)
 {
    // build heapify
+   int largest=root ;
+   int left = 2*root + 1;
+   int right = 2*root + 2;
+   if (left < n && arr[left] > arr[largest])
+   {
+     largest = left;
+   }
+   
+   if (right<n && arr[right] > arr[largest])
+   {
+     largest = right;
+   }
+    if (largest != root)
+    {
+      swap(&arr[root], &arr[largest]);
+      heapify(arr, n, largest);
+    }
   
    
 }
@@ -13,10 +40,15 @@ void heapify(int arr[], int n, int root)
 void heapSort(int arr[], int n)
 {
    // build heap
-   
+   for (int i=n/2-1;i>=0;i--){
+     heapify(arr,n,i);
+   }
   
    // extracting elements from heap one by one
-   
+   for (int i=n-1;i>=0;i--){
+     swap(&arr[0],&arr[i]);
+     heapify(arr,i,0);
+   }
 }
   
 /* print contents of array */
@@ -29,9 +61,16 @@ void displayArray(int arr[], int n)
   
 // main program
 int main()
-{
-   int heap_arr[] = {4,17,3,12,9,6};
-   int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
+{  int n;
+cout<<"Enter the number of elements in the array"<<endl;
+cin>>n;
+cout<<"Enter the elements of the array"<<endl;
+int heap_arr[n];
+for(int i=0;i<n;i++){
+    cin>>heap_arr[i];
+}
+  // int heap_arr[] = {4,17,3,12,9,6};
+   //int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
    cout<<"Input array"<<endl;
    displayArray(heap_arr,n);
   
